17f7f540cb503870c92723c9484a7f84b7ff3bef
Andrey Karpov <andrewcarpov@gmail.com>
2015-12-23 18:09:42 +0300

    Configure nuget package build

---
+++ build.cmd
@@ -0,0 +1,21 @@
+@echo off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=0.5.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=src\MicroFlow\.nuget\nuget.exe
+)
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild" src\MicroFlow\MicroFlow.sln /t:Rebuild /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+set package_id="MicroFlow"
+
+%nuget% pack "src\MicroFlow\MicroFlow.nuspec" -sym -NoPackageAnalysis -Version %version% -Properties "Configuration=%config%;PackageId=%package_id%"
--- src/MicroFlow/MicroFlow/Properties/AssemblyInfo.cs
+++ src/MicroFlow/MicroFlow/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("MicroWorkflow")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MicroFlow")]
+[assembly: AssemblyDescription("Lightweight framework to create workflows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MicroWorkflow")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Andrey Karpov")]
+[assembly: AssemblyProduct("MicroFlow")]
+[assembly: AssemblyCopyright("Copyright ©  2015, Andrey Karpov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
